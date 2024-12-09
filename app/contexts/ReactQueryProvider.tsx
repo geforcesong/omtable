70bb5232b2729163158da9ae5d7250d843c35252
geforcesong <georgeguo@outlook.com>
2024-12-09 10:41:54 +0800

    updated

---
+++ app/contexts/ReactQueryProvider.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { PropsWithChildren, useState } from "react";
+
+const ReactQueryProvider: React.FC<PropsWithChildren> = ({ children }) => {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default ReactQueryProvider;
